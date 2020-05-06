740a7e3cd5c5604513cfbc05f9e3c10421e7852e
AndreyBelichenko <andreybe@wizardsdev.com>
2020-05-06 16:54:30 +0300

    add gitignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "38ab8ec0ade302012dbc";
+/******/ 	var hotCurrentHash = "61dd398add4757f45f6b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
